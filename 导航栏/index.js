5838dc662ad117dc3dccc521e8ff202444005738
zhangooy <44726378+zhangooy@users.noreply.github.com>
2020-10-14 12:07:12 +0800

    导航栏案例

---
+++ 导航栏/index.js
@@ -0,0 +1,19 @@
+  window.onload = function(){
+    // 1. 获取元素
+        var nav = document.querySelector('.nav');
+        var lis = nav.children; // 得到4个小li
+        // 2.循环注册事件
+        // children[1]里面索引1代表的意思是 children(li)有两个孩子 a 和ul  ul索引就1
+        for (var i = 0; i < lis.length; i++) {
+            lis[i].onmouseover = function() {
+                this.children[1].style.display = 'block';
+            }
+            lis[i].onmouseout = function() {
+                this.children[1].style.display = 'none';
+            }
+        }
+  }
+
+
+
+  
